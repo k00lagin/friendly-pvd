3a85fbb1d3ecd5afc367f885c3f1481fd0228f0f
Vsevolod Kulagin <k00lagin@gmail.com>
2020-07-28 01:46:22 +0500

    Указал данные приложения

--- src/index.js
+++ src/index.js
@@ -16,8 +16,9 @@ if (require('electron-squirrel-startup')) {
 const createWindow = () => {
 	// Create the browser window.
 	const mainWindow = new BrowserWindow({
-		width: 800,
-		height: 600,
+		width: 1024,
+		height: 768,
+		icon: '../public/icon.png',
 		webPreferences: {
 			nodeIntegration: true
 		}
