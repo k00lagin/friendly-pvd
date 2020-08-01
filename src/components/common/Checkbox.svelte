8005f770f0894b2d6d1c7d8bb48b60adc05efe66
Vsevolod Kulagin <k00lagin@gmail.com>
2020-08-01 20:57:03 +0500

    Поменял иконки на чекбоксах

--- src/components/common/Checkbox.svelte
+++ src/components/common/Checkbox.svelte
@@ -1,9 +1,11 @@
 <script>
+	import Icon from '@common/Icon.svelte';
 	export let name = '';
 	export let checked = false;
 </script>
 
 <label>
-	<input type="checkbox" bind:checked={checked} name={name}>
+	<input type="checkbox" bind:checked={checked} name={name} class="visually-hidden">
+	<Icon size="18" type={checked?'checkbox-outline':'checkbox-blank-outline'} style="vertical-align: sub;"></Icon>
 	<slot></slot>
 </label>
