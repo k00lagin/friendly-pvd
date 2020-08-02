4c93f016f548266b5f0f806037a4c6379413cee4
Vsevolod Kulagin <k00lagin@gmail.com>
2020-08-02 21:37:25 +0500

    Позволить отключать чекбоксы

--- src/components/common/Checkbox.svelte
+++ src/components/common/Checkbox.svelte
@@ -2,10 +2,11 @@
 	import Icon from '@common/Icon.svelte';
 	export let name = '';
 	export let checked = false;
+	export let disabled = false;
 </script>
 
 <label>
-	<input type="checkbox" bind:checked={checked} name={name} class="visually-hidden">
+	<input type="checkbox" bind:checked={checked} name={name} class="visually-hidden" disabled={disabled}>
 	<Icon size="18" type={checked?'checkbox-outline':'checkbox-blank-outline'} style="vertical-align: sub;"></Icon>
 	<slot></slot>
 </label>
